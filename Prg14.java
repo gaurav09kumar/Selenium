8ce9a80dd99fb71bd8f1c3a0e9ecc77575785c91
Gaurav Kumar Rai <34624495+gaurav09kumar@users.noreply.github.com>
2019-07-16 11:04:05 +0530

    Create Prg14.java

---
+++ Prg14.java
@@ -0,0 +1,31 @@
+//action class
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class Prg9 {
+	private static final WebElement AdminView = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\VSPICT\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://opensource-demo.orangehrmlive.com");
+		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@id='txtUsername']/following::input[1]")).sendKeys("admin123");
+		driver.findElement(By.xpath("//input[@id='txtUsername']/following::input[2]")).click();
+
+		Actions builder = new Actions(driver);
+		WebElement AdminView = driver.findElement(By.id("menu_admin_viewAdminModule"));
+
+		Action moveoverhome = builder.moveToElement(AdminView).build();
+		moveoverhome.perform();
+
+		driver.findElement(By.xpath("//b[contains(text(),'PIM')]")).click();
+	}
+}
